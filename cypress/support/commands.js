c8f7f473d2173536d38973ebbcb935ca3e3afe2d
Julio Guerra <j.guerratamayo@gmail.com>
2023-05-21 17:37:29 +0200

    Add testing library and reall events to commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import "@testing-library/cypress/add-commands";
+import "cypress-real-events";
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
