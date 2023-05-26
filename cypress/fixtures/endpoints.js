56dffa6bd99a6ce7f259b2d010a6d35b66840dfb
Julio Guerra <j.guerratamayo@gmail.com>
2023-05-26 23:43:58 +0200

    Add TestData and Endpoints URLs

---
+++ cypress/fixtures/endpoints.js
@@ -0,0 +1,11 @@
+export const contentEndPoint =
+  /^.*\/pub\/static\/version.*\/frontend\/Magento\/luma\/en_US\/Magento_Checkout\/template\/minicart\/content\.html$/;
+
+export const getCartEndPoint =
+  /https:\/\/magento\.softwaretestingboard\.com\/customer\/section\/load\//;
+
+export const shippingPriceEndPoint =
+  /^https:\/\/magento\.softwaretestingboard\.com\/pub\/static\/version\d+\/frontend\/Magento\/luma\/en_US\/Magento_Tax\/template\/checkout\/shipping_method\/price\.html$/;
+
+export const billingAddressEndPoint =
+  /.*\/pub\/static\/version\d+\/frontend\/Magento\/luma\/en_US\/Magento_Checkout\/template\/billing-address\/actions\.html/;
